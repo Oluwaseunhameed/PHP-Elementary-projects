5fdf88c01bb2c9c02640f716a2cb9079c87e330b
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-06-10 13:44:15 +0100

    file upload procession in php 7.4

---
+++ file_upload.php
@@ -0,0 +1,39 @@
+<?php
+// Create $errorMessage variable
+$errorMessage = '';
+// Check if file was uploaded and save it locally
+if (isset($_FILES['file'])) {
+    // Get the file extension
+    $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+    // Convert $ext into lowercase
+    $ext = strtolower($ext);
+
+    // Check if the file size is more than 5MB and show error
+    if ($_FILES['file']['size'] > 5 * 1024 * 2014) {
+      $errorMessage = 'File size can not be more than 5MB';
+      // Display error message in HTML
+    } elseif (!in_array($ext, ['jpg', 'png', 'jpeg', 'svg'])) {
+      $errorMessage = 'You can only upload images';
+    } elseif ($_FILES['file']['error'] === 0) {
+        move_uploaded_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+    }
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<p><?php echo $errorMessage ?></p>
+<form action="" method="post" enctype="multipart/form-data">
+  <input type="file" name="file"><br>
+  <button>Submit</button>
+</form>
+</body>
+</html>
